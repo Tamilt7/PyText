9a977347300e6982d12a1ad122510e1783bf6775
Tamilt7 <33990857+Tamilt7@users.noreply.github.com>
2021-05-22 22:03:02 +0530

    commit

--- main.py
+++ main.py
@@ -11,3 +11,25 @@ textbox.pack()
 
 window.configure(bg="blue")
 window.mainloop()
+
+# File menu
+
+
+def cmdNew():
+    pass
+
+
+def cmdOpen():
+    pass
+
+
+def cmdSave():
+    pass
+
+
+def cmdSaveas():
+    pass
+
+
+def cmdExit():
+    pass
